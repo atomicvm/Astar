6ee81515fa838344449c8e1e20026764efc60b01
Shunsuke Watanabe <ww.shunsuke@gmail.com>
2022-05-02 11:17:35 +0900

    shibuya treasury & governance

--- bin/collator/src/parachain/chain_spec/shibuya.rs
+++ bin/collator/src/parachain/chain_spec/shibuya.rs
@@ -4,9 +4,9 @@ use cumulus_primitives_core::ParaId;
 use sc_service::ChainType;
 use shibuya_runtime::{
     wasm_binary_unwrap, AccountId, AuraConfig, AuraId, Balance, BalancesConfig, BaseFeeConfig,
-    BlockRewardConfig, CollatorSelectionConfig, EVMConfig, GenesisConfig, ParachainInfoConfig,
-    Precompiles, SessionConfig, SessionKeys, Signature, SudoConfig, SystemConfig, VestingConfig,
-    SDN,
+    BlockRewardConfig, CollatorSelectionConfig, CouncilConfig, DemocracyConfig, EVMConfig,
+    GenesisConfig, ParachainInfoConfig, Precompiles, SessionConfig, SessionKeys, Signature,
+    SudoConfig, SystemConfig, TechnicalCommitteeConfig, TreasuryConfig, VestingConfig, SDN,
 };
 use sp_core::{sr25519, Pair, Public};
 
@@ -136,6 +136,24 @@ fn make_genesis(
             sp_runtime::Permill::from_parts(125_000),
         ),
         ethereum: Default::default(),
+        council: CouncilConfig {
+            members: vec![
+                get_account_id_from_seed::<sr25519::Public>("Alice"),
+                get_account_id_from_seed::<sr25519::Public>("Bob"),
+                get_account_id_from_seed::<sr25519::Public>("Charlie"),
+            ],
+            phantom: Default::default(),
+        },
+        technical_committee: TechnicalCommitteeConfig {
+            members: vec![
+                get_account_id_from_seed::<sr25519::Public>("Alice"),
+                get_account_id_from_seed::<sr25519::Public>("Bob"),
+                get_account_id_from_seed::<sr25519::Public>("Charlie"),
+            ],
+            phantom: Default::default(),
+        },
+        democracy: DemocracyConfig::default(),
+        treasury: TreasuryConfig::default(),
     }
 }
 
